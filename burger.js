612b4d6ab8241354db4b9945cc9e138cf24e2d8d
boody <bodymagdy97@gmail.com>
2024-02-13 01:18:47 +0200

    batman

---
+++ burger.js
@@ -0,0 +1,44 @@
+// Fetch the JSON file
+fetch('/burger.json')
+  .then(response => response.json()) // Parse the JSON response
+  .then(data => {
+    // Handle the data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error('Error fetching the JSON file:', error);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
